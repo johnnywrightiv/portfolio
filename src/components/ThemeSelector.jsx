28f820d9d835760b748d6c26a39a06ab7d8c586e
Johnny Wright <johnnywrightiv@gmail.com>
2024-06-22 01:26:16 -0500

    simplified ThemeSelector rendering

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -7,56 +7,18 @@ import useBlur from '../utils/useBlur';
 const ThemeSelector = ({ isMobile = false }) => {
   const { theme, changeTheme, systemTheme, availableThemes } = useThemeContext();
   const [isOpen, setIsOpen] = useState(false);
-
   const effectiveTheme = theme === 'system' ? systemTheme : theme;
-
   const closeMenu = useCallback(() => setIsOpen(false), []);
   const menuRef = useBlur(closeMenu);
+  const ThemeIcon = effectiveTheme === 'dark' ? FiSun : FiMoon;
 
   const handleChangeTheme = (newTheme) => {
     changeTheme(newTheme);
     setIsOpen(false);
   };
 
-  const ThemeIcon = effectiveTheme === 'dark' ? FiSun : FiMoon;
-
-  if (isMobile) {
-    return (
-      <div className="w-[75%]" ref={menuRef}>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="w-full flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 my-2 p-4 cursor-pointer"
-        >
-          <div className="w-8 flex justify-center">
-            <ThemeIcon size={25} />
-          </div>
-          <span className="pl-4">Theme</span>
-        </button>
-
-        {isOpen && (
-          <div className=" w-full mt-2 p-2 rounded-md flex flex-col justify-center items-center">
-            {availableThemes.map(t => (
-              <button
-                key={t}
-                onClick={() => handleChangeTheme(t)}
-                className={`w-[85%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-1 p-3 cursor-pointer ${
-                  theme === t ? 'bg-purple-200' : ''
-                }`}
-              >
-                <div className="w-8 flex justify-center">
-                  {t === 'dark' ? <FiMoon size={20} /> : t === 'system' ? <FiSettings size={20} /> : <FiSun size={20} />}
-                </div>
-                <span className="pl-4">{capitalize(t)}</span>
-              </button>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
-
   return (
-    <div className="md:block hidden fixed top-4 z-10" ref={menuRef}>
+    <div className={isMobile ? "fixed left-4 top-4 z-10" : "md:block hidden fixed top-4 z-10"} ref={menuRef}>
       <div className="relative">
         <button
           onClick={() => setIsOpen(!isOpen)}
