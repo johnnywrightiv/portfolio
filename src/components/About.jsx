5f8b1809206837ac17ee2412a8fd886024714922
Johnny Wright <johnnywrightiv@gmail.com>
2024-06-24 19:18:48 -0500

    minor color changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import TechBadge from './TechBadge';
-// import { FaHtml5, FaCss3Alt, FaJsSquare, FaBootstrap, FaReact, FaNodeJs, FaDatabase, FaCode, FaServer, FaLink, FaPython, FaNode, FaFigma, FaGitAlt } from 'react-icons/fa';
-import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs, FaDatabase, FaBootstrap, FaPython, FaFigma, FaGitAlt, } from 'react-icons/fa';
-import { SiMongodb, SiJquery, SiVercel, SiNextdotjs, SiExpress, SiAdobephotoshop } from "react-icons/si";
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaNodeJs, FaDatabase, FaBootstrap, FaPython, FaFigma, FaGitAlt } from 'react-icons/fa';
+import { SiMongodb, SiRedux, SiJquery, SiVercel, SiNextdotjs, SiExpress, SiAdobephotoshop } from "react-icons/si";
 import { VscVscodeInsiders } from "react-icons/vsc";
-
-
 
 
 const About = () => {
   return (
-    <div id='about' className="sm:h-screen bg-orange-300 flex flex-col items-center justify-center p-8">
+    <div id='about' className="md:h-screen bg-orange-300 flex flex-col items-center justify-center p-8">
       <h1 className="text-5xl font-bold text-htag mb-6 text-center">
         Hi, I'm <span className="block md:hidden p-2"></span> John Wright
       </h1>
@@ -23,10 +20,12 @@ const About = () => {
         <p className="text-lg text-ptag mb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quo incidunt magni quae ipsa, minus repellat quasi deleniti, in aspernatur perferendis, ducimus fuga. Alias expedita eum voluptas autem modi. Quam dignissimos voluptatibus quidem adipisci veniam sit. Totam fugit, voluptate quidem minima debitis rem ut beatae.</p>
         <h2 className="text-3xl font-bold text-htag mb-4">My Tech Stack</h2>
         <div className="flex flex-wrap justify-center">
-          <TechBadge text="HTML5" icon={FaHtml5} color="bg-orange-400" />
-          <TechBadge text="CSS3" icon={FaCss3Alt} color="bg-blue-400" />
-          <TechBadge text="JavaScript" icon={FaJsSquare} color="bg-yellow-400" />
-          <TechBadge text="React" icon={FaReact} color="bg-cyan-400" />
+
+          {/* <TechBadge text="HTML5" icon={FaHtml5}" />
+          <TechBadge text="CSS3" icon={FaCss3Alt}" />
+          <TechBadge text="JavaScript" icon={FaJsSquare}" />
+          <TechBadge text="React" icon={FaReact}" />
+          <TechBadge text="Redux" icon={FaRedux}" />
           <TechBadge text="Next.js" icon={SiNextdotjs} />
           <TechBadge text="Node.js" icon={FaNodeJs} />
           <TechBadge text="Express" icon={SiExpress} />
@@ -36,11 +35,32 @@ const About = () => {
           <TechBadge text="Bootstrap" icon={FaBootstrap} />
           <TechBadge text="Python" icon={FaPython} />
           <TechBadge text="jQuery" icon={SiJquery} />
-          <TechBadge text="Vercel" icon={SiVercel}/>
+          <TechBadge text="Vercel" icon={SiVercel} />
           <TechBadge text="Figma" icon={FaFigma} />
-          <TechBadge text="Photoshop" icon={SiAdobephotoshop}/>
+          <TechBadge text="Photoshop" icon={SiAdobephotoshop} />
           <TechBadge text="Git & GitHub" icon={FaGitAlt} />
-          <TechBadge text="VSCode" icon={VscVscodeInsiders} />
+          <TechBadge text="VSCode" icon={VscVscodeInsiders} /> */}
+
+          <TechBadge text="HTML5" icon={FaHtml5} color="bg-[#E34F26]" />
+          <TechBadge text="CSS3" icon={FaCss3Alt} color="bg-[#1572B6]" />
+          <TechBadge text="JavaScript" icon={FaJsSquare} color="bg-[#F7DF1E]" />
+          <TechBadge text="React" icon={FaReact} color="bg-[#61DAFB]" />
+          <TechBadge text="Redux" icon={SiRedux} color="bg-[#764ABC]" />
+          <TechBadge text="Next.js" icon={SiNextdotjs} color="bg-[#000000]" />
+          <TechBadge text="Node.js" icon={FaNodeJs} color="bg-[#339933]" />
+          <TechBadge text="Express" icon={SiExpress} color="bg-[#000000]" />
+          <TechBadge text="MongoDB" icon={SiMongodb} color="bg-[#47A248]" />
+          <TechBadge text="NoSQL" icon={FaDatabase} color="bg-[#A6E22E]" />
+          <TechBadge text="Tailwind" icon={FaCss3Alt} color="bg-[#38B2AC]" />
+          <TechBadge text="Bootstrap" icon={FaBootstrap} color="bg-[#7952B3]" />
+          <TechBadge text="Python" icon={FaPython} color="bg-[#3776AB]" />
+          <TechBadge text="jQuery" icon={SiJquery} color="bg-[#0769AD]" />
+          <TechBadge text="Vercel" icon={SiVercel} color="bg-[#000000]" />
+          <TechBadge text="Figma" icon={FaFigma} color="bg-[#F24E1E]" />
+          <TechBadge text="Photoshop" icon={SiAdobephotoshop} color="bg-[#31A8FF]" />
+          <TechBadge text="Git & GitHub" icon={FaGitAlt} color="bg-[#F05032]" />
+          <TechBadge text="VSCode" icon={VscVscodeInsiders} color="bg-[#007ACC]" />
+
         </div>
       </div>
     </div>
