b9c37765699d7720f44f274a8a1d07b86b5b22bf
Johnny Wright <johnnywrightiv@gmail.com>
2024-07-31 15:29:55 -0500

    color consistency in About

--- src/utils/determineTextColorClass.js
+++ src/utils/determineTextColorClass.js
@@ -2,6 +2,6 @@ export const determineTextColorClass = (dynamicColor) => {
   if (dynamicColor) {
     return dynamicColor === '#000000' ? 'text-primary' : 'text-dynamic'
   } else {
-    return 'text-tertiary'
+    return 'text-cta'
   }
 }
