f914eb953c7dadc5e7c94f884fe06934dfa39a01
Johnny Wright <johnnywrightiv@gmail.com>
2024-11-22 18:51:44 -0600

    fix navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@
 import About from '@/components/about';
 import Contact from '@/components/contact';
 import Hero from '@/components/hero';
-import Navbar from '@/components/navbar';
 import Projects from '@/components/projects';
 
 const page = () => {
 	return (
 		<div>
-			{/* <Navbar /> */}
 			<Hero />
 			<About />
 			<Projects />
